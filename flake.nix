71368a69ee083e95bc7ea492cbc5b432e986c8c6
sfriedenberg-etsy <sfriedenberg@etsy.com>
2026-06-25 15:27:23 -0400

    bump nixpkgs-master: d233902 → 567a49d

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     # (pkgs.buildGoApplication, pkgs.mkGoEnv, pkgs.gomod2nix CLI). See
     # `man 7 gomod2nix` inside the devshell for the migration guide.
     igloo.url = "github:amarbel-llc/igloo";
-    nixpkgs-master.url = "github:NixOS/nixpkgs/d233902339c02a9c334e7e593de68855ad26c4cb";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/567a49d1913ce81ac6e9582e3553dd90a955875f";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
     madder = {
       url = "github:amarbel-llc/madder";
