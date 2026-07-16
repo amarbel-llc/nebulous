1b7fc0519ace68db82a6f9fc9c03e6008347e03b
friedenberg <1baggzpr@starbrandshoes.com>
2026-07-16 12:48:03 +0000

    bump nixpkgs-master: d233902 → 567a49d
    madder: a7f7e7f → 22d6c97
    purse-first: 4d3361c → 2c7eac2

--- flake.nix
+++ flake.nix
@@ -5,11 +5,11 @@
     # amarbel-llc/nixpkgs carries the gomod2nix build helpers natively
     # (pkgs.buildGoApplication, pkgs.mkGoEnv, pkgs.gomod2nix CLI). See
     # `man 7 gomod2nix` inside the devshell for the migration guide.
-    igloo.url = "git+https://github.com/amarbel-llc/igloo.git";
+    igloo.url = "git+https://code.linenisgreat.com/igloo.git";
     nixpkgs-master.url = "github:NixOS/nixpkgs/567a49d1913ce81ac6e9582e3553dd90a955875f";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
     madder = {
-      url = "git+https://github.com/amarbel-llc/madder.git";
+      url = "git+https://code.linenisgreat.com/madder.git";
       inputs.igloo.follows = "igloo";
       inputs.nixpkgs-master.follows = "nixpkgs-master";
       inputs.utils.follows = "utils";
@@ -21,7 +21,7 @@
       inputs.utils.follows = "utils";
     };
     purse-first = {
-      url = "git+https://github.com/amarbel-llc/purse-first.git";
+      url = "git+https://code.linenisgreat.com/purse-first.git";
       inputs.igloo.follows = "igloo";
       inputs.nixpkgs-master.follows = "nixpkgs-master";
       inputs.utils.follows = "utils";
@@ -56,7 +56,7 @@
     bob.inputs.purse-first.inputs.utils.follows = "utils";
     bob.inputs.tap.inputs.utils.follows = "utils";
     conformist = {
-      url = "git+https://github.com/amarbel-llc/conformist.git";
+      url = "git+https://code.linenisgreat.com/conformist.git";
       inputs.igloo.follows = "igloo";
       inputs.nixpkgs-master.follows = "nixpkgs-master";
       inputs.utils.follows = "utils";
